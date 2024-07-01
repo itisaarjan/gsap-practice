c559d489dbda210f51446ac26e2da8f7c3c09fc7
itisaarjan <arjansubedi@usf.edu>
2024-07-01 13:59:47 -0400

    Practice

--- script.js
+++ script.js
@@ -15,9 +15,29 @@ t1.from(".page-1-b img",{
     start: "top 50px", // when the top of the box hits the center of the viewport
     end: "top ", // when the top of the box hits 100px from the top of the viewport
     scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-    markers: true // enables markers to visualize start/end positions
+    // markers: true // enables markers to visualize start/end positions
   }
-
-
+})
+t1.from(".type--A",{
+    x:-500,
+    duration:2,
+    scrollTrigger:{
+        trigger:".type--A",
+        start:"top 60%",
+        end:"top",
+        scrub:true,
+        // markers:true
+    }
+})
+t1.to(".page-2 h1",{
+    x:-2200,
+    duration:2,
+    scrollTrigger:{
+        trigger:".page-2",
+        start:"top 30%",
+        end:"top",
+        scrub:true,
+        // markers:true
+    }
 })
 
